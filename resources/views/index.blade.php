298633eb69ea5b0852ab9fc5f293b7e5ffa244e9
Surawal <143913818+SurAwall17@users.noreply.github.com>
2026-05-01 15:20:34 +0800

    Nav & Banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,7 @@
 
 <body>
     <header class="bg-[#ED153E] h-[50vh]">
-        <nav
-            class="fixed w-full py-5 px-7 md:px-14 flex justify-between">
+        <nav class="fixed w-full py-5 px-7 md:px-14 flex justify-between">
             {{-- logo --}}
             <div class="logo">
                 <img src="{{ asset('images/logo-white.png') }}" class="w-36 md:w-48" alt="Logo Moody">
@@ -24,8 +23,15 @@
                 <li class="menu"><a href="/order">Order</a></li>
                 <li class="menu"><a href="/about">About</a></li>
                 <li class="menu"><a href="/notification">Notifications</a></li>
-                <li class="menu cursor-pointer"><x-heroicon-s-user-circle
+                {{-- <li class="menu cursor-pointer"><x-heroicon-s-user-circle
                         class="w-8 text-white  hover:text-[#157aedd4] transition-all duration-300 ease-in-out" />
+                </li> --}}
+                <li class="">
+                    <a href="/login">
+                        <Button class="btn-login flex px-3 py-2 bg-white text-[#ED153E] rounded-2xl font-semibold">Login
+                            <x-heroicon-o-arrow-left-end-on-rectangle class="w-5 me-1" />
+                        </Button>
+                    </a>
                 </li>
             </ul>
 
